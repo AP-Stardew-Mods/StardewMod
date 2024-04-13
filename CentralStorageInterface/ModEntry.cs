5fdfaf9dd0f8d322bc649b73716e4c4930561cfa
alecrm <alec.millar94@gmail.com>
2024-04-12 23:27:34 -0400

    Added base project files

---
+++ CentralStorageInterface/ModEntry.cs
@@ -0,0 +1,17 @@
+﻿using StardewModdingAPI;
+
+namespace CentralStorageInterface;
+
+internal class ModEntry : Mod
+{
+    /*********
+        ** Public methods
+        *********/
+    /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+    /// <param name="helper">Provides simplified APIs for writing mods.</param>
+    public override void Entry(IModHelper helper)
+    {
+        // TODO: Do stuff
+    }
+
+}
