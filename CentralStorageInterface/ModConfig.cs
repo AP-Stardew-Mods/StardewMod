471473616e9a0c1cd0bdbca1e1c687d03b1d60e1
Patrick King <pat.k195@gmail.com>
2024-04-16 11:41:48 -0400

    added a content.json for future

---
+++ CentralStorageInterface/ModConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Mod Config Class
+
+namespace CentralStorageInterface
+{
+    class ModConfig
+    {
+
+        //TODO: Add Config
+    }
+}
