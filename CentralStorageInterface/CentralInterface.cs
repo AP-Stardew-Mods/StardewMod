ae4de2a93ac632ca9b27a6ecab99c1349c4ce360
Patrick King <pat.k195@gmail.com>
2024-04-16 14:46:27 -0400

    Moved assests and object to CP folder, created api, central interface, Icontent classes

---
+++ CentralStorageInterface/CentralInterface.cs
@@ -0,0 +1,23 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralStorageInterface
+{
+    internal class CentralInterface
+    {
+
+        static IModHelper Helper;
+        static IMonitor Monitor;
+
+        internal static void Initialize(IMod ModInstance)
+        {
+            Helper = ModInstance.Helper;
+            Monitor = ModInstance.Monitor;
+            
+        }
+    }
+}
---
+++ CentralStorageInterface/Utils.cs
@@ -0,0 +1,29 @@
+﻿using StardewValley;
+using StardewValley.Locations;
+using StardewValley.Objects;
+using StardewValley.TerrainFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI.Utilities;
+using Netcode;
+
+namespace CentralStorageInterface
+{
+    
+    internal static class Log
+    {
+        internal static void Error(string msg) => ModEntry.ModMonitor.Log(msg, StardewModdingAPI.LogLevel.Error);
+        internal static void Alert(string msg) => ModEntry.ModMonitor.Log(msg, StardewModdingAPI.LogLevel.Alert);
+        internal static void Warn(string msg) => ModEntry.ModMonitor.Log(msg, StardewModdingAPI.LogLevel.Warn);
+        internal static void Info(string msg) => ModEntry.ModMonitor.Log(msg, StardewModdingAPI.LogLevel.Info);
+        internal static void Debug(string msg) => ModEntry.ModMonitor.Log(msg, StardewModdingAPI.LogLevel.Debug);
+        internal static void Trace(string msg) => ModEntry.ModMonitor.Log(msg, StardewModdingAPI.LogLevel.Trace);
+        internal static void Verbose(string msg) => ModEntry.ModMonitor.VerboseLog(msg);
+
+    }
+    
+}
