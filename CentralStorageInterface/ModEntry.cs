e8fb2b7c0eb4598d19ea5d1310a164bd78fbcef6
alecrm <alec.millar94@gmail.com>
2025-07-26 16:17:05 -0400

    Re-reformat

--- CentralStorageInterface/ModEntry.cs
+++ CentralStorageInterface/ModEntry.cs
@@ -27,7 +27,7 @@ internal sealed class ModEntry : Mod
         helper.Events.Input.ButtonPressed += this.OnButtonPressed;
 
         // Helper for object list change. Example placing an object
-         helper.Events.World.ObjectListChanged += ObjectListChanged;
+        helper.Events.World.ObjectListChanged += ObjectListChanged;
     }
 
 
@@ -39,53 +39,53 @@ internal sealed class ModEntry : Mod
         // Print button presses to console
         this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
 
-         if (e.Button.IsActionButton())
+        if (e.Button.IsActionButton())
         {
-             Vector2 tile = e.Cursor.Tile;
+            Vector2 tile = e.Cursor.Tile;
 
-             if (IsInterfaceOnTile(tile, out StardewValley.Object interfaceObject))
+            if (IsInterfaceOnTile(tile, out StardewValley.Object interfaceObject))
             {
 
 
-                  this.Monitor.Log("Terminal", LogLevel.Debug);
-                  Interface.OpenInterface(interfaceObject);
+                this.Monitor.Log("Terminal", LogLevel.Debug);
+                Interface.OpenInterface(interfaceObject);
 
 
                 // Test Menu
-                  Vector2 center = Utility.getTopLeftPositionForCenteringOnScreen(800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2);
-                  Interface_Menu testMenu = new Interface_Menu(this.Monitor, (int)center.X, (int)center.Y, 800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2, true);
-                  Game1.activeClickableMenu = testMenu;
+                Vector2 center = Utility.getTopLeftPositionForCenteringOnScreen(800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2);
+                Interface_Menu testMenu = new Interface_Menu(this.Monitor, (int)center.X, (int)center.Y, 800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2, true);
+                Game1.activeClickableMenu = testMenu;
 
 
 
             }
-         }
+        }
 
     }
 
     // Credit: UltimateStorage System
     private bool IsInterfaceOnTile(Vector2 tile, out StardewValley.Object interfaceObject)
     {
-         return (Game1.currentLocation.objects.TryGetValue(tile, out interfaceObject) && interfaceObject.Name == "Central Interface") ||
-               (Game1.currentLocation.objects.TryGetValue(tile + new Vector2(0, 1), out interfaceObject) && interfaceObject.Name == "Central Interface");
+        return (Game1.currentLocation.objects.TryGetValue(tile, out interfaceObject) && interfaceObject.Name == "Central Interface") ||
+              (Game1.currentLocation.objects.TryGetValue(tile + new Vector2(0, 1), out interfaceObject) && interfaceObject.Name == "Central Interface");
     }
 
 
 
     private void ObjectListChanged(object? sender, ObjectListChangedEventArgs e)
     {
-         Monitor.Log($"Placed object at {e.Location}");
+        Monitor.Log($"Placed object at {e.Location}");
 
-         foreach (var pair in e.Added)
+        foreach (var pair in e.Added)
         {
-             Vector2 tile = pair.Key;
-             StardewValley.Object obj = pair.Value;
+            Vector2 tile = pair.Key;
+            StardewValley.Object obj = pair.Value;
 
-             Monitor.Log($"Changed object {obj.Name}");
+            Monitor.Log($"Changed object {obj.Name}");
 
-             if (obj.name == "Central Interface")
+            if (obj.name == "Central Interface")
             {
-                  Interface testobj = new Interface(obj.name, tile);
+                Interface testobj = new Interface(obj.name, tile);
             }
         }
 
