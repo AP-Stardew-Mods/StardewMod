11204129ba9439b8626dbb539a4d50da5fdcd3e2
alecrm <alec.millar94@gmail.com>
2025-07-26 16:32:43 -0400

    one more formatting thing for Interface_Menu

--- CentralStorageInterface/Classes/Objects/Interface_Menu.cs
+++ CentralStorageInterface/Classes/Objects/Interface_Menu.cs
@@ -21,9 +21,7 @@ public class Interface_Menu : StardewValley.Menus.IClickableMenu
 
     //public Interface_Menu(IMonitor monitor) : base()
     //{
-
     //	monitor.Log("Menu Constructed", LogLevel.Debug);
-
     //}
 
     public Interface_Menu(IMonitor monitor, int x, int y, int width, int height, bool showUpperRightCloseButton = false) : base()
@@ -31,12 +29,17 @@ public class Interface_Menu : StardewValley.Menus.IClickableMenu
         monitor.Log("Better Menu Constructed", LogLevel.Debug);
         inventory = new InventoryMenu(xPositionOnScreen + IClickableMenu.spaceToClearSideBorder + IClickableMenu.borderWidth,
                                       yPositionOnScreen + IClickableMenu.spaceToClearTopBorder + IClickableMenu.borderWidth, playerInventory: true);
-        trashCan = new ClickableTextureComponent(new Rectangle(xPositionOnScreen + width / 3 + 576 + 32,
-                                                               yPositionOnScreen + IClickableMenu.borderWidth + IClickableMenu.spaceToClearTopBorder + 192 + 64, 64, 104),
-                                                 Game1.mouseCursors,
-                                                 new Rectangle(564 + Game1.player.trashCanLevel * 18, 102, 18, 26),
-                                                 4f);
-
+        trashCan = new ClickableTextureComponent(
+            new Rectangle(
+              xPositionOnScreen + width / 3 + 576 + 32,
+              yPositionOnScreen + IClickableMenu.borderWidth + IClickableMenu.spaceToClearTopBorder + 192 + 64,
+              64,
+              104
+            ),
+            Game1.mouseCursors,
+            new Rectangle(564 + Game1.player.trashCanLevel * 18, 102, 18, 26),
+            4f
+        );
     }
 
 
@@ -57,7 +60,6 @@ public class Interface_Menu : StardewValley.Menus.IClickableMenu
         base.draw(b);
         inventory.draw(b);
         trashCan.draw(b);
-
     }
 
 }
