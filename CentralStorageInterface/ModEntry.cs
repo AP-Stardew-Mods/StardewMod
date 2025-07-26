39f65404e0fe22c0635efc853957bafb76f18831
alecrm <alec.millar94@gmail.com>
2025-07-26 16:18:22 -0400

    re-re-reformatted

--- CentralStorageInterface/ModEntry.cs
+++ CentralStorageInterface/ModEntry.cs
@@ -15,14 +15,12 @@ internal sealed class ModEntry : Mod
 
 
     /*********
- ** Public methods
- *********/
+     ** Public methods
+     *********/
     /// <summary>The mod entry point, called after the mod is first loaded.</summary>
     /// <param name="helper">Provides simplified APIs for writing mods.</param>
     public override void Entry(IModHelper helper)
     {
-
-
         //helper.Events.Content.AssetRequested += this.OnAssetRequested;
         helper.Events.Input.ButtonPressed += this.OnButtonPressed;
 
@@ -46,22 +44,18 @@ internal sealed class ModEntry : Mod
             if (IsInterfaceOnTile(tile, out StardewValley.Object interfaceObject))
             {
 
-
                 this.Monitor.Log("Terminal", LogLevel.Debug);
                 Interface.OpenInterface(interfaceObject);
-
 
                 // Test Menu
                 Vector2 center = Utility.getTopLeftPositionForCenteringOnScreen(800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2);
                 Interface_Menu testMenu = new Interface_Menu(this.Monitor, (int)center.X, (int)center.Y, 800 + IClickableMenu.borderWidth * 2, 600 + IClickableMenu.borderWidth * 2, true);
                 Game1.activeClickableMenu = testMenu;
 
-
-
             }
+
         }
 
-    }
 
     // Credit: UltimateStorage System
     private bool IsInterfaceOnTile(Vector2 tile, out StardewValley.Object interfaceObject)
@@ -69,7 +63,6 @@ internal sealed class ModEntry : Mod
         return (Game1.currentLocation.objects.TryGetValue(tile, out interfaceObject) && interfaceObject.Name == "Central Interface") ||
               (Game1.currentLocation.objects.TryGetValue(tile + new Vector2(0, 1), out interfaceObject) && interfaceObject.Name == "Central Interface");
     }
-
 
 
     private void ObjectListChanged(object? sender, ObjectListChangedEventArgs e)
@@ -88,7 +81,6 @@ internal sealed class ModEntry : Mod
                 Interface testobj = new Interface(obj.name, tile);
             }
         }
-
 
     }
 
