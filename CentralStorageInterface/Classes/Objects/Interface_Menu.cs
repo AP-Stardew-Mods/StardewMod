a4ac84382b82e6c8ba8f4fc3d5d6fe5f520d37f9
pkingGMU <pking23@gmu.edu>
2025-07-31 18:22:25 -0400

    structs for components x,y,w,h relative to intitial drawn component

--- CentralStorageInterface/Classes/Objects/Interface_Menu.cs
+++ CentralStorageInterface/Classes/Objects/Interface_Menu.cs
@@ -31,6 +31,41 @@ public class Interface_Menu : StardewValley.Menus.IClickableMenu
     public int customBoxWidth;
     public int customBoxHeight;
     public Texture2D testTexture; 
+    public Texture2D itemFrame;
+
+    // Alias
+    public int vX;
+    public int vY;
+    public int vW;
+    public int vH;
+    
+    // Relative Values //
+
+
+    // BG IClickableMenu //
+    public struct bgClickable {
+        public int x, y, w, h;
+    }
+
+    public bgClickable bgClick;
+
+    // BG Texture //
+    public struct bgTexture {
+        public int x, y, w, h;
+        public Texture2D texture; 
+    }
+
+    public bgTexture bgText;
+    
+    // Middle Third Rectangle 
+    public struct middle_third_rect {
+        public int x, y, w, h;
+    }
+
+    public middle_third_rect mid_third_rect;
+
+
+    
     
     public Interface_Menu(IMonitor monitor, IModHelper helper, int x, int y, int width, int height, bool showUpperRightCloseButton = false) : base()
     {
@@ -48,7 +83,7 @@ public class Interface_Menu : StardewValley.Menus.IClickableMenu
             new Rectangle(564 + Game1.player.trashCanLevel * 18, 102, 18, 26),
             4f
         );
-
+        
         // TODO make these percentages not hardcoded values
         
         // Position of Background and Foreground
@@ -58,13 +93,19 @@ public class Interface_Menu : StardewValley.Menus.IClickableMenu
         this.posY = this.bgPosY + 12;
 
         // Width and Height of Background and Foreground
-        this.bgWidth = Game1.viewport.Width - 10;
-        this.bgHeight = Game1.viewport.Height - 10;
+        this.bgWidth = Game1.viewport.Width - 100;
+        this.bgHeight = Game1.viewport.Height - 100;
         this.customBoxWidth = this.bgWidth - 24;
         this.customBoxHeight = this.bgHeight - 24;
 
         //this.testTexture = Game1.content.Load<Texture2D>("assets/items/BigCraftables/Central Interface.png");
-        this.testTexture = helper.ModContent.Load<Texture2D>("assets/Central Interface.png");
+        //this.testTexture = helper.ModContent.Load<Texture2D>("assets/cat.png");
+
+        calculateTextureValues();
+
+        
+        // Item Frame Init
+        this.itemFrame = helper.ModContent.Load<Texture2D>("assets/menu/item_frame.png");
     }
 
 
@@ -79,20 +120,52 @@ public class Interface_Menu : StardewValley.Menus.IClickableMenu
         _updateTimer -= (float)time.ElapsedGameTime.TotalSeconds;
     }
 
+    public void calculateTextureValues()
+    {
+        // Alias
+        vX = Game1.viewport.X;
+        vY = Game1.viewport.Y;
+        vW = Game1.viewport.Width;
+        vH = Game1.viewport.Height;
+
+        // bgClick
+        bgClick.x = 0;
+        bgClick.y = 0;
+        bgClick.w = vW;
+        bgClick.h = vH;
+
+        // bgText
+        bgText.x = bgClick.x + 12;
+        bgText.y = bgClick.y + 12;
+        bgText.w = bgClick.w - 24;
+        bgText.h = bgClick.h - 24;
+
+        // mid_third_rect
+        mid_third_rect.x = bgText.w / 3;
+        mid_third_rect.y = bgText.y;
+        mid_third_rect.w = mid_third_rect.x;
+        mid_third_rect.h = bgText.h;
+    }
 
     public override void draw(SpriteBatch b)
     {
         base.draw(b);
 
+        
+        
+        
         // Drawing Stuff //
         // https://learn.microsoft.com/en-us/previous-versions/windows/xna/bb196426(v=xnagamestudio.10) //
 
-        IClickableMenu.drawTextureBox(b, this.bgPosX, this.bgPosY, this.bgWidth, this.bgHeight, Color.White);
-        b.Draw(Game1.staminaRect, new Rectangle(this.posX, this.posY, this.customBoxWidth, this.customBoxHeight), Color.Black);
+        IClickableMenu.drawTextureBox(b, bgClick.x, bgClick.y, bgClick.w, bgClick.h, Color.White);
+        b.Draw(Game1.staminaRect, new Rectangle(mid_third_rect.x, mid_third_rect.y, mid_third_rect.w, mid_third_rect.h), Color.Red);
 
+        // Draw Item Frame //
+        
+        
         // Test Texture Draw //
-        int test_scale = 4;
-        b.Draw(this.testTexture, new Rectangle(100, 100, 16 * test_scale, 32 * test_scale), Color.White);
+        //int test_scale = 4;
+        //b.Draw(this.testTexture, new Rectangle(this.posX, this.posY, this.customBoxWidth, this.customBoxHeight), Color.White);
         
         this.drawMouse(b);
 
